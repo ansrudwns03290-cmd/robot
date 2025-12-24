2bdef67ece1db2615cb34ff8ed86e657e47173b2
KyungJunMoon <ansrudwns03290@gmail.com>
2025-12-24 10:38:44 +0900

    1st example

--- day1/day1_1.c
+++ day1/day1_1.c
@@ -2,8 +2,11 @@
 
 int main(void){
     
-    printf("Be happy");
-    printf("My friend");
+    printf("Be happy\n");
+    printf("12345678901234567890\n");
+    printf("My\tfriend\n");
+    printf("Goot\bd\tchance\n");
+    printf("Cow\rW\a\n");
     
     return 0;
 }
