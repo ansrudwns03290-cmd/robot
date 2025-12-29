0f04617dd2af4e8b6953a773ead03558bb62886d
KyungJunMoon <ansrudwns03290@gmail.com>
2025-12-29 13:53:54 +0900

    example

--- day3/day3_3.c
+++ day3/day3_3.c
@@ -1,40 +1,39 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(void)
-{
-    int N, m, G, W;
-    float t, R, SF, DM;
-    float c = 0.018, g = 9.81;
-    float f_min, f_motor;
+int main(void){
+    double mass, torque, gear, radius, SF, margin;
+    int wheels;
 
-    printf("Input N: ");
-    scanf("%d", &N);
-    printf("Input m: ");
-    scanf("%d", &m);
-    printf("Input SF: ");
-    scanf("%f", &SF);
-    printf("Input DM: ");
-    scanf("%f", &DM);
-    printf("Input R: ");
-    scanf("%f", &R);
-    printf("Input t: ");
-    scanf("%f", &t);
-    printf("Input G: ");
-    scanf("%d", &G);
-    W = m * g;
-    f_min = c * W * SF * DM;
-    f_motor = t * G * N / R;
+    const double g = 9.81;
+    const double c = 0.018;
 
-    printf("총 하중(kg): %d\n", m);
-    printf("구동 바퀴 수: %d\n", N);
-    printf("모터 정격 토크(N*m): %f\n", t);
-    printf("감속비: %d\n", G);
-    printf("바퀴 반지름(m): %d\n\n", m);
-    printf("필요한 최소 견인력: %f\n", f_min);
-    printf("모터가 낼 수 있는 견인력: %f\n", f_motor);
-    
-    if(f_min <= f_motor)
-        printf("판정: 만족\n");
-    else
-        printf("판정: 불만족 (더 큰 모터나 감속비가 필요합니다.)\n");
+    printf("총 하중(kg) : ");
+    scanf("%lf", &mass);
+    printf("구동 바퀴 수 : ");
+    scanf("%d", &wheels);
+    printf("안전계수 : ");
+    scanf("%lf", &SF);
+    printf("설계마진(20% == 1.2로 입력) : ");
+    scanf("%lf", &margin);
+
+    printf("모터 정격 토크(N*m) : ");
+    scanf("%lf", &torque);
+    printf("감속비 : ");
+    scanf("%lf", &gear);
+    printf("바퀴 반지름(m) : ");
+    scanf("%lf", &radius);
+
+    double W = mass * g;
+    double F_min = c * W * SF * margin;
+    double F_motor = (torque * gear / radius) * wheels;
+
+    printf("\n필요한 최소 견인력 : %.2f [N]\n", F_min);
+    printf("모터가 낼 수 있는 견인력 : %.2f [N]\n", F_motor);
+
+    if (F_motor >= F_min){
+        printf("판정 : 만족 (모터 견인력이 충분합니다.)\n");
+    } else {
+        printf("판정 : 불만족 (더 큰 모터나 감속비가 필요.)\n");
+    }
+    return 0;
 }
